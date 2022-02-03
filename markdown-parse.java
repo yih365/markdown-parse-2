b80d53310938eee10a88539d19d76dae30b23598
yih365 <68409635+yih365@users.noreply.github.com>
2022-02-03 10:32:27 -0800

    Create markdown-parse.java

---
+++ markdown-parse.java
@@ -0,0 +1,40 @@
+// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class MarkdownParse {
+    public static ArrayList<String> getLinks(String markdown) throws IOException {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+
+            //checks for infinite looping
+            if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+                break;
+            }
+
+            //checks for brackets and parentheses with stuff between them, and empty links
+            if (nextCloseBracket + 1 == openParen && openParen + 1 != closeParen) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            
+            currentIndex = closeParen + 1;
+        }
+        return toReturn;
+    }
+    public static void main(String[] args) throws IOException {
+		Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+        System.out.println(links);
+    }
+}
