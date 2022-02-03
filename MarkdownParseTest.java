f26e8f0aa7f07154536ecb02043c45e61121f6d9
yih365 <68409635+gigi365@users.noreply.github.com>
2022-02-03 10:47:10 -0800

    fixed junit file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -25,11 +25,11 @@ public class MarkdownParseTest {
 
         filename = Path.of("test-file3.md");
         contents = Files.readString(filename);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+        assertEquals(MarkdownParse.getLinks(contents), List.of());
 
         filename = Path.of("test-file4.md");
         contents = Files.readString(filename);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+        assertEquals(MarkdownParse.getLinks(contents), List.of("a link on the first line"));
     }
 
     /*
